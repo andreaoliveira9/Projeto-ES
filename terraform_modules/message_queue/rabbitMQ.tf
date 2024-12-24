b259f259b03e04bf08bebeb9fac6720666316861
André Oliveira <andreaoliveira@ua.pt>
2024-12-24 01:45:41 +0000

    fix

--- terraform_modules/message_queue/rabbitMQ.tf
+++ terraform_modules/message_queue/rabbitMQ.tf
@@ -1,12 +1,3 @@
-resource "aws_mq_subnet_group" "mq_subnet_group" {
-  name       = "mq_subnet_group"
-  subnet_ids = [var.private_subnet_ids[0], var.private_subnet_ids[1]]
-
-  tags = {
-    Name = "MQ Subnet Group"
-  }
-}
-
 resource "aws_mq_broker" "mq" {
   broker_name               = "mq-primary"
   engine_type               = "RabbitMQ"
@@ -14,7 +5,8 @@ resource "aws_mq_broker" "mq" {
   host_instance_type        = "mq.m5.large"
   security_groups           = [aws_security_group.mq_sg.id]
   deployment_mode           = "ACTIVE_STANDBY_MULTI_AZ"
-  subnet_ids                = aws_mq_subnet_group.mq_subnet_group.subnet_ids
+  subnet_ids                = var.private_subnet_ids
+  publicly_accessible       = false
 
   user {
     username = vsr.mq_user
