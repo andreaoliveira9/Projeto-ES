e32269df4109c39528c2038913795a93eb31f3f0
André Oliveira <andreaoliveira@ua.pt>
2024-12-24 01:52:13 +0000

    Update output.tf

--- terraform_modules/message_queue/output.tf
+++ terraform_modules/message_queue/output.tf
@@ -1,4 +1,4 @@
 output "mq_connection_string" {
-  value       = "amqps://${aws_mq_broker.mq.id}.mq.${aws_region.current.name}.amazonaws.com:5671"
+  value       = "amqps://${aws_mq_broker.mq.id}.mq.${var.aws_region.name}.amazonaws.com:5671"
   description = "Connection string for RabbitMQ"
 }
