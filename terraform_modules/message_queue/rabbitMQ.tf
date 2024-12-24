d2ec10eb7a5eec61d3338e1e87d1026bb56fd16a
André Oliveira <andreaoliveira@ua.pt>
2024-12-24 02:15:03 +0000

    Update rabbitMQ.tf

--- terraform_modules/message_queue/rabbitMQ.tf
+++ terraform_modules/message_queue/rabbitMQ.tf
@@ -1,12 +1,12 @@
 resource "aws_mq_broker" "mq" {
-  broker_name               = "mq-primary"
-  engine_type               = "RabbitMQ"
-  engine_version            = "3.13"
-  host_instance_type        = "mq.m5.large"
-  security_groups           = [aws_security_group.mq_sg.id]
-  deployment_mode           = "CLUSTER_MULTI_AZ"
-  subnet_ids                = var.private_subnet_ids
-  publicly_accessible       = false
+  broker_name                = "mq"
+  engine_type                = "RabbitMQ"
+  engine_version             = "3.13"
+  host_instance_type         = "mq.m5.large"
+  security_groups            = [aws_security_group.mq_sg.id]
+  deployment_mode            = "CLUSTER_MULTI_AZ"
+  subnet_ids                 = var.private_subnet_ids
+  publicly_accessible        = false
   auto_minor_version_upgrade = true
 
   user {
