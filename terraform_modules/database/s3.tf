57997be2682ef4c4fd96f555293226da2e35ca63
André Oliveira <andreaoliveira@ua.pt>
2024-12-31 00:06:35 +0100

    Update s3.tf

--- terraform_modules/database/s3.tf
+++ terraform_modules/database/s3.tf
@@ -11,7 +11,7 @@ resource "aws_s3_bucket_ownership_controls" "image_bucket_ownership" {
   bucket = aws_s3_bucket.image_bucket.id
 
   rule {
-    object_ownership = "BucketOwnerEnforced"
+    object_ownership = "ObjectWriter"
   }
 }
 
